00f473bd0922126ac6ad4d245df63efb6928a327
MahaAldawood9 <ma8279@nyu.edu>
2025-05-06 13:26:10 -0400

    draft 3

--- scripts.js
+++ scripts.js
@@ -10,79 +10,103 @@ const mapOptions = {
 const map = new mapboxgl.Map(mapOptions);
 
 map.on('load', () => {
-    const boroughs = {
-        mn: './data/mn.geojson',
-        bk: './data/bk.geojson',
-        qn: './data/qn.geojson',
-        si: './data/si.geojson',
-        bx: './data/bx.geojson'
-    };
-
-    for (const id in boroughs) {
-        map.addSource(id, {
-            type: 'geojson',
-            data: boroughs[id]
-        });
-
-        map.addLayer({
-            id: id,
-            type: 'circle',
-            source: id,
-            paint: {
-                'circle-radius': 6,
-                'circle-color': '#999', 
-                'circle-opacity': 0.2
-            }
-        });
-    }
-});
-
-function highlightBorough(id) {
-    const boroughs = ['mn', 'bk', 'qn', 'si', 'bx'];
-    selectedBoroughId = id;
-
-    boroughs.forEach(b => {
-        if (b === id) {
-            map.setPaintProperty(b, 'circle-color', '#ff6600');
-            map.setPaintProperty(b, 'circle-opacity', 0.6);
-        } else {
-            map.setPaintProperty(b, 'circle-color', '#999999');
-            map.setPaintProperty(b, 'circle-opacity', 0.2);
-        }
+    map.addSource('deaths', {
+        type: 'geojson',
+        data: './data/deaths.geojson'
     });
 
-    // Hide the sidebar and show the filter container
-    document.getElementById('sidebar-container').style.display = 'block'; 
-    document.getElementById('filter-container').style.display = 'block';
-    document.getElementById('selected-borough-label').innerText = boroughNameFromId(id);
-
-    map.setFilter(id, null); 
-}
-
-function boroughNameFromId(id) {
-    const names = {
-        mn: 'Manhattan',
-        bk: 'Brooklyn',
-        qn: 'Queens',
-        si: 'Staten Island',
-        bx: 'The Bronx'
-    };
-    return names[id];
-}
+    map.addLayer({
+        id: 'crash-points',
+        type: 'circle',
+        source: 'deaths',
+        paint: {
+            'circle-radius': 6,
+            'circle-color': '#999999', // default gray
+            'circle-opacity': 0.3
+        }
+    });
+});
 
 function filterCrashes(property) {
-    if (!selectedBoroughId) return;
-    map.setFilter(selectedBoroughId, ['>', ['get', property], 1]);
+    activeProperty = property;
+
+    map.setPaintProperty('crash-points', 'circle-color', [
+        'case',
+        ['>', ['get', property], 0], '#ff6600', // highlight match
+        '#999999' // others gray
+    ]);
+
+    map.setPaintProperty('crash-points', 'circle-opacity', [
+        'case',
+        ['>', ['get', property], 0], 0.9,
+        0.2
+    ]);
 }
 
-function goBack() {
-    if (selectedBoroughId) {
-        map.setPaintProperty(selectedBoroughId, 'circle-color', '#999999');
-        map.setPaintProperty(selectedBoroughId, 'circle-opacity', 0.2);
-        map.setFilter(selectedBoroughId, null);
+function resetFilter() {
+    activeProperty = null;
+
+    map.setPaintProperty('crash-points', 'circle-color', '#999999');
+    map.setPaintProperty('crash-points', 'circle-opacity', 0.6);
+}
+
+
+map.on('click', 'crash-points', (e) => {
+    const props = e.features[0].properties;
+  
+    let incidentType = '';
+    let numberOfKilled = 0;
+  
+    // Determine if it's a cyclist or pedestrian incident
+    if (props.number_of_cyclist_killed > 0) {
+      incidentType = 'Cyclist';
+      numberOfKilled = props.number_of_cyclist_killed;
+    } else if (props.number_of_pedestrians_killed > 0) {
+      incidentType = 'Pedestrian';
+      numberOfKilled = props.number_of_pedestrians_killed;
     }
+  
+    const html = `
+    ${numberOfKilled} ${incidentType} Killed due to ${props.contributing_factor_vehicle_1} on ${props.crash_date}
+ 
+    `;
+  
+    new mapboxgl.Popup()
+      .setLngLat(e.lngLat)
+      .setHTML(html)
+      .addTo(map);
+  });
 
-    selectedBoroughId = null;
-    document.getElementById('sidebar-container').style.display = 'block'; 
-    document.getElementById('filter-container').style.display = 'none';
+
+  function filterByContributingFactor(factor) {
+    selectedFactor = factor;
+
+    // Update the filter to show only features with the selected contributing factor
+    map.setFilter('crash-points', ['==', ['get', 'contributing_factor_vehicle_1'], selectedFactor]);
+
+    // Highlight the selected factor points in dark red
+    map.setPaintProperty('crash-points', 'circle-color', '#ff0000'); // Dark Red for selected
+    map.setPaintProperty('crash-points', 'circle-opacity', 0.8);
 }
+
+
+
+//     map.addLayer({
+//         id: 'crash-points',
+//         type: 'circle',
+//         source: 'deaths',
+//         paint: {
+//             'circle-radius': 6,
+//             'circle-color': '#ff6600',
+//             'circle-opacity': 0.6
+//         }
+//     });
+// });
+
+// function filterCrashes(property) {
+//     map.setFilter('crash-points', ['>', ['get', property], 0]);
+// }
+
+// function resetFilter() {
+//     map.setFilter('crash-points', null);
+// }
